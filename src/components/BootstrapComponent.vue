511d58aea22907f2025c0929837b40620f88df2f
hypons <hypons@me.com>
2023-09-17 15:11:04 +0800

    added a simple  component to bootstrap page

---
+++ src/components/BootstrapComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is an Bootstrap component.</h1>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class BootstrapComponent extends Vue {}
+</script>
---
+++ src/views/Bootstrap.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>This is an Bootstrap page</h1>
+  <BootstrapComponent />
+</template>
+    
+    
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import BootstrapComponent from "@/components/BootstrapComponent.vue";
+
+@Options({
+  components: {
+    BootstrapComponent,
+  },
+})
+export default class Bootstrap extends Vue {}
+</script>
